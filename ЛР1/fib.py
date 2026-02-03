1fe4fc85ce99e897e9211f784a37a14569e4a5f7
Leonid Tots <59145792+huksleva@users.noreply.github.com>
2026-02-03 16:19:21 +0300

    123

--- ЛР1/fib.py
+++ ЛР1/fib.py
@@ -27,28 +27,30 @@ class fib_gen:
         """
         return a
 
+
 class fib_gen_simplified:
-    """Класс-итератор чисел Фибоначи (упрощённая версия через __getitem__)"""
+    """Класс для доступа к числам Фибоначчи по индексу"""
 
     def __init__(self):
-        self.a = 0
-        self.b = 1
-        self.sequence = list()
-
-
-    # Нужно, чтобы объект (класс) стал итератором, чтобы дальше мы смогли обращаться к нему как к итератору
-    def __iter__(self):
-        return self
+        self.sequence = [0, 1]  # Начальные значения
 
     def __getitem__(self, index):
-        while index >= len(self.sequence):
-            self.sequence.append(self.a)
-            self.b += self.a
-            self.a = self.b - self.a
+        if index < 0:
+            raise IndexError("Negative indices not supported")
+
+        # Генерируем последовательность до нужного индекса
+        while len(self.sequence) <= index:
+            next_val = self.sequence[-1] + self.sequence[-2]
+            self.sequence.append(next_val)
+
         return self.sequence[index]
 
-
-
+    def __iter__(self):
+        # Для поддержки итерации - используем генератор
+        i = 0
+        while True:
+            yield self[i]
+            i += 1
 
 def fibonacchi_gen():
     """Генератор чисел Фибоначи"""
@@ -61,6 +63,14 @@ def fibonacchi_gen():
         a = b
         b = res
 
+def fibonacchi_korutina():
+    """Сопрограмма (корутина) чисел Фибоначи"""
+    index = yield
+    a = 0
+    b = 1
+    for _ in range(index):
+        a, b = b, a + b
+    yield a
 
 
 
@@ -82,20 +92,29 @@ def fibonacchi_gen():
 
 
 if __name__ == '__main__':
+    n = 10
     print("Числа фибоначи через итератор\n\nОбычный вариант:")
     x = fib_gen()
-    for _ in range(10):
+    for _ in range(n):
         print(next(x))
 
     print("\nУпрощённый вариант:")
     y = fib_gen_simplified()
-    for i in range(10):
+    for i in range(n):
         print(y[i])
 
+    print("\n\nЧисла фибоначи через генератор и сопрограмму:")
+    print("\nГенератор:")
+    g = fibonacchi_gen()
+    for i in range(n):
+        print(next(g))
 
+    print("\nЧерез корутину:")
 
-
-
+    for i in range(n):
+        h = fibonacchi_korutina()
+        next(h)
+        print(h.send(i))
 
 
 
--- ЛР1/test_fib.py
+++ ЛР1/test_fib.py
@@ -1,16 +1,93 @@
 import unittest
-from fib import Fibonacchilst
-
-class Test(unittest.TestCase):
-
-    def test_normal(self):
+from fib import fib_gen, fib_gen_simplified, fibonacchi_gen, fibonacchi_korutina
 
 
+class TestFibonacciImplementations(unittest.TestCase):
+    """Тесты для всех реализаций чисел Фибоначчи."""
+
+    def setUp(self):
+        """Подготовка ожидаемой последовательности Фибоначчи."""
+        self.expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+        self.test_length = len(self.expected)
+
+    def test_fib_gen_class(self):
+        """Тест класса-итератора fib_gen."""
+        fib_iter = fib_gen()
+        result = [next(fib_iter) for _ in range(self.test_length)]
+        self.assertEqual(result, self.expected)
+
+    def test_fib_gen_simplified_class(self):
+        """Тест упрощённого класса fib_gen_simplified."""
+        fib_obj = fib_gen_simplified()
+        result_indexed = [fib_obj[i] for i in range(self.test_length)]
+        self.assertEqual(result_indexed, self.expected)
+
+    def test_fibonacchi_gen_function(self):
+        """Тест генераторной функции fibonacchi_gen."""
+        fib_gen_func = fibonacchi_gen()
+        result = [next(fib_gen_func) for _ in range(self.test_length)]
+        self.assertEqual(result, self.expected)
+
+    def test_fibonacchi_korutina_function(self):
+        """Тест сопрограммы fibonacchi_korutina."""
+        for i in range(self.test_length):
+            coro = fibonacchi_korutina()
+            next(coro)
+            result = coro.send(i)
+            self.assertEqual(result, self.expected[i])
+
+    def test_edge_cases(self):
+        """Тест граничных случаев."""
+        # Тест для индекса 0
+        self.assertEqual(next(fib_gen()), 0)
+        self.assertEqual(fib_gen_simplified()[0], 0)
+        self.assertEqual(next(fibonacchi_gen()), 0)
+
+        coro_0 = fibonacchi_korutina()
+        next(coro_0)
+        self.assertEqual(coro_0.send(0), 0)
+
+        # Тест для индекса 1
+        fib_iter = fib_gen()
+        next(fib_iter)
+        self.assertEqual(next(fib_iter), 1)
+
+        self.assertEqual(fib_gen_simplified()[1], 1)
+
+        fib_gen_func = fibonacchi_gen()
+        next(fib_gen_func)
+        self.assertEqual(next(fib_gen_func), 1)
+
+        coro_1 = fibonacchi_korutina()
+        next(coro_1)
+        self.assertEqual(coro_1.send(1), 1)
+
+    def test_consistency_across_implementations(self):
+        """Тест согласованности всех реализаций между собой."""
+        n = 15
+
+        # Создаём ОДИН экземпляр каждого типа
+        fib_iter = fib_gen()
+        result1 = [next(fib_iter) for _ in range(n)]
+
+        fib_simpl = fib_gen_simplified()
+        result2 = [fib_simpl[i] for i in range(n)]
+
+        fib_gen_func = fibonacchi_gen()
+        result3 = [next(fib_gen_func) for _ in range(n)]
+
+        result4 = []
+        for i in range(n):
+            coro = fibonacchi_korutina()
+            next(coro)
+            result4.append(coro.send(i))
+
+        expected_n = self.expected[:n]
+        self.assertEqual(result1, expected_n)
+        self.assertEqual(result2, expected_n)
+        self.assertEqual(result3, expected_n)
+        self.assertEqual(result4, expected_n)
 
 
-
-
-
-
-
-
+if __name__ == '__main__':
+    unittest.main()
