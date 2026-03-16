60e8d1f2bd1ad7612795080ff496cc29f75957ee
Leonid <leonid005xc@gmail.com>
2026-03-16 12:32:13 +0300

    123

--- ЛР3/main.py
+++ ЛР3/main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
 from sqlalchemy.orm import sessionmaker
 from database import engine, create_db_and_tables
+from contextlib import asynccontextmanager
+
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+
+
+@app.post("/users/")
+async def create_user():
+    pass
+
+
 
 
 if __name__ == "__main__":
