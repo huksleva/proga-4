c29e8f4abfe6b95024873894166325d9347afa58
Leonid <leonid005xc@gmail.com>
2026-03-16 14:44:22 +0300

    123

--- ЛР3/main.py
+++ ЛР3/main.py
@@ -14,21 +14,6 @@ app = FastAPI(lifespan=lifespan)
 
 
 
-@app.post("/users/")
-async def create_user():
-    pass
-
-
-
-
-if __name__ == "__main__":
-    #Session = sessionmaker(engine)
-
-    #with Session as session:
-    #    pass
-
-    create_db_and_tables()
-
 
 
 
